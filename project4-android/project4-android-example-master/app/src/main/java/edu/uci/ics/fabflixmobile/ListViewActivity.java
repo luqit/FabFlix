a6a32823db1de44abafd1d8bfa25e218749ccc72
jabodey <nguyenk8@uci.edu>
2018-06-02 04:36:31 -0700

    the latest and greatest fabflixmobile 2.5 beta

--- project4-android/project4-android-example-master/app/src/main/java/edu/uci/ics/fabflixmobile/ListViewActivity.java
+++ project4-android/project4-android-example-master/app/src/main/java/edu/uci/ics/fabflixmobile/ListViewActivity.java
@@ -3,18 +3,23 @@ package edu.uci.ics.fabflixmobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 
 import java.util.ArrayList;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class ListViewActivity extends Activity {
-    private String offset = "";
+    private Integer offset = 0;
     private String jsonString = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +28,8 @@ public class ListViewActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         Toast.makeText(this, "MOOOOOOOOOVIES", Toast.LENGTH_LONG).show();
 
-        offset = bundle.getString("offset");
+        offset = bundle.getInt("offset");
         jsonString = bundle.getString("jsonArrayString");
-
         String movieId = "";
         String movieTitle = "";
         String movieYear = "";
@@ -37,7 +41,7 @@ public class ListViewActivity extends Activity {
         if (jsonString != null && !"".equals(jsonString)) {
             try {
                 JSONArray jsonArray = new JSONArray(jsonString);
-                for (int x = 0; x < jsonArray.length(); x++) {
+                for (int x = offset; x < jsonArray.length(); x++) {
                     movieId = jsonArray.getJSONObject(x).getString("movieId");
                     movieTitle = jsonArray.getJSONObject(x).getString("movieTitle");
                     movieYear = jsonArray.getJSONObject(x).getString("movieYear");
@@ -46,7 +50,7 @@ public class ListViewActivity extends Activity {
                     genreNames = jsonArray.getJSONObject(x).getString("genreNames");
                     rating = jsonArray.getJSONObject(x).getString("rating");
                     movies.add(new Movie(movieId, movieTitle, movieYear, movieDirector, genreNames, starNames, rating));
-                    if (jsonArray.length() > 5 && (x == 4)) {
+                    if ((jsonArray.length() > offset+5) && (x == (offset+4))) {
                         break;
                     }
 
@@ -56,7 +60,6 @@ public class ListViewActivity extends Activity {
             }
 
             MovieListViewAdapter adapter = new MovieListViewAdapter(movies, this);
-
             ListView listView = (ListView) findViewById(R.id.list);
             listView.setAdapter(adapter);
 
@@ -64,12 +67,35 @@ public class ListViewActivity extends Activity {
     }
 
     public void goToNextPage(View view) {
-        Intent goToIntent = new Intent(this, Pagination.class);
+        Intent goToIntent = new Intent(this, ListViewActivity.class);
 
         goToIntent.putExtra("jsonArrayString", jsonString);
-        goToIntent.putExtra("offset", 5);
+        goToIntent.putExtra("offset", offset+5);
 
         startActivity(goToIntent);
 
+    }
+
+    public void goToPreviousPage(View view) {
+        Intent goToIntent = new Intent(this, ListViewActivity.class);
+
+        if((offset-5) < 0)
+        {
+            Toast.makeText(getApplicationContext(), "There ain't no movies in the previous page, man!", LENGTH_SHORT).show();
+            return;
+        }
+        goToIntent.putExtra("jsonArrayString", jsonString);
+        goToIntent.putExtra("offset", offset-5);
+
+        startActivity(goToIntent);
+
+    }
+
+    public void goToSingleMoviePage(View view){
+        Intent goToIntent = new Intent(this, SingleMovieActivity.class);
+        Log.d("ID", ((TextView)view.findViewById(R.id.movieTitle)).getText().toString());
+        goToIntent.putExtra("movieTitle", ((TextView)view.findViewById(R.id.movieTitle)).getText().toString());
+        goToIntent.putExtra("jsonArrayString", jsonString);
+        startActivity(goToIntent);
     }
 }
